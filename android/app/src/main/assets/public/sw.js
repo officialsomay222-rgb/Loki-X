27c90b1afae32e0110c880e7dc65b0f67086b6d7
Owner <officialsomay222@gmail.com>
2026-07-18 16:11:38 +0530

    build: fix gradle execution and update dependencies
    Ensure gradlew has execute permissions before building the APK in CI and update Capacitor dependencies to version 8.4.2.

--- android/app/src/main/assets/public/sw.js
+++ android/app/src/main/assets/public/sw.js
@@ -1,1 +1,135 @@
-if(!self.define){let e,s={};const n=(n,i)=>(n=new URL(n+".js",i).href,s[n]||new Promise(s=>{if("document"in self){const e=document.createElement("script");e.src=n,e.onload=s,document.head.appendChild(e)}else e=n,importScripts(n),s()}).then(()=>{let e=s[n];if(!e)throw new Error(`Module ${n} didn’t register its module`);return e}));self.define=(i,t)=>{const o=e||("document"in self?document.currentScript.src:"")||location.href;if(s[o])return;let r={};const l=e=>n(e,o),a={module:{uri:o},exports:r,require:l};s[o]=Promise.all(i.map(e=>a[e]||l(e))).then(e=>(t(...e),r))}}define(["./workbox-4e9e9954"],function(e){"use strict";self.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"test.html",revision:"bee801bb53dc775d8dbc9d787126da8a"},{url:"logo.png",revision:"c71aa2b74fe3625523218e8c66e976c0"},{url:"index.html",revision:"a7d6b6854da7aaaaf2ee90e6c850ddff"},{url:"app-icon.png",revision:"44b6b0da9448f73745bab352e8064dd8"},{url:"assets/workbox-window.prod.es5-BBnX5xw4.js",revision:null},{url:"assets/index-PYRl10CK.css",revision:null},{url:"assets/index-B4xKst-x.js",revision:null},{url:"assets/howler-CxYKD8rI.js",revision:null},{url:"app-icon.png",revision:"44b6b0da9448f73745bab352e8064dd8"},{url:"manifest.webmanifest",revision:"bae28b62a948393f8888c3712ead9736"}],{}),e.cleanupOutdatedCaches(),e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("/index.html"))),e.registerRoute(/^https:\/\/i\.ibb\.co\/.*/i,new e.CacheFirst({cacheName:"external-images",plugins:[new e.ExpirationPlugin({maxEntries:50,maxAgeSeconds:2592e3}),new e.CacheableResponsePlugin({statuses:[0,200]})]}),"GET"),e.registerRoute(/^https:\/\/fonts\.googleapis\.com\/.*/i,new e.StaleWhileRevalidate({cacheName:"google-fonts-stylesheets",plugins:[]}),"GET"),e.registerRoute(/^https:\/\/fonts\.gstatic\.com\/.*/i,new e.CacheFirst({cacheName:"google-fonts-webfonts",plugins:[new e.ExpirationPlugin({maxEntries:30,maxAgeSeconds:31536e3}),new e.CacheableResponsePlugin({statuses:[0,200]})]}),"GET")});
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-b1bafff1'], (function (workbox) { 'use strict';
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "test.html",
+    "revision": "bee801bb53dc775d8dbc9d787126da8a"
+  }, {
+    "url": "logo.png",
+    "revision": "a1d0b7b4c285226eb55b9b08d4ae5f58"
+  }, {
+    "url": "index.html",
+    "revision": "d12f06731321d90b3ffe1ce74ec90fac"
+  }, {
+    "url": "app-icon.png",
+    "revision": "8a35c69c35a99c9100778d441248da79"
+  }, {
+    "url": "assets/workbox-window.prod.es5-BBnX5xw4.js",
+    "revision": null
+  }, {
+    "url": "assets/index-s0D8aZXZ.css",
+    "revision": null
+  }, {
+    "url": "assets/index-GMMAymkW.js",
+    "revision": null
+  }, {
+    "url": "assets/howler-CvZzNgBo.js",
+    "revision": null
+  }, {
+    "url": "app-icon.png",
+    "revision": "8a35c69c35a99c9100778d441248da79"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "bae28b62a948393f8888c3712ead9736"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html")));
+  workbox.registerRoute(/^https:\/\/i\.ibb\.co\/.*/i, new workbox.CacheFirst({
+    "cacheName": "external-images",
+    plugins: [new workbox.ExpirationPlugin({
+      maxEntries: 50,
+      maxAgeSeconds: 2592000
+    }), new workbox.CacheableResponsePlugin({
+      statuses: [0, 200]
+    })]
+  }), 'GET');
+  workbox.registerRoute(/^https:\/\/fonts\.googleapis\.com\/.*/i, new workbox.StaleWhileRevalidate({
+    "cacheName": "google-fonts-stylesheets",
+    plugins: []
+  }), 'GET');
+  workbox.registerRoute(/^https:\/\/fonts\.gstatic\.com\/.*/i, new workbox.CacheFirst({
+    "cacheName": "google-fonts-webfonts",
+    plugins: [new workbox.ExpirationPlugin({
+      maxEntries: 30,
+      maxAgeSeconds: 31536000
+    }), new workbox.CacheableResponsePlugin({
+      statuses: [0, 200]
+    })]
+  }), 'GET');
+
+}));
